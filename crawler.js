0f7444f658f6b4b294c325e1fcc5820cd03744f9
Trey Kane <treykane@gmail.com>
2020-01-22 10:16:49 -0500

    Building out crawler params

---
+++ crawler.js
@@ -0,0 +1,43 @@
+const SitemapGenerator = require('sitemap-generator');
+
+const baseUrl = 'https://redbookjars.com'
+// create the sitemap generator
+const generator = SitemapGenerator(baseUrl, {
+    httpsAgent: https.globalAgent,
+    maxDepth: 0,
+    filepath: './sitemap.xml',
+    maxEntriesPerFile: 50000,
+    stripQuerystring: true,
+    ignoreAMP: true,
+    lastMod: true,
+    priorityMap: [1.0, 0.5, 0.2, 0],
+    ignore: url => {
+        // Prevent URLs from being added that contain `<pattern>`.
+        return /<pattern>/g.test(url)
+    }
+});
+
+
+// register event listeners
+generator.on('done', () => {
+    // sitemaps created
+    console.log('SITEMAP CRAWL COMPLETE!')
+  });
+
+  generator.on('add', (url) => {
+    // log url
+    console.log('Mapped: ', url)
+  });
+
+generator.on('error', (error) => {
+    // => { code: 404, message: 'Not found.', url: 'http://example.com/foo' }
+    console.log(error);
+});
+
+generator.on('ignore', (url) => {
+    // log ignored url
+    console.log('Robots told me to ignore: ', url)
+  });
+
+  // start the crawler
+  generator.start();
